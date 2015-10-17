88578cd2b1974e3abbc64df6e8e33e8636ad5f2e
Konstatninos Patsourakos <sdi0900129@di.uoa.gr>
2015-10-17 09:43:37 +0300

    variable done, equation init

---
+++ equation.h
@@ -0,0 +1,16 @@
+#ifndef EQUATION_H_20ESQHLL
+#define EQUATION_H_20ESQHLL
+
+#include <iostream>
+
+class equation
+{
+public:
+	equation (std::string);
+	virtual ~equation ();
+
+private:
+	/* data */
+};
+
+#endif /* end of include guard: EQUATION_H_20ESQHLL */
